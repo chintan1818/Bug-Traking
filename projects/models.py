94b18101a92a462358f92d068f4622c0122fb62a
Yash Brahmkshatriya <yash311000@gmail.com>
2021-10-16 14:45:25 +0530

    Registering models and adding sample data

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,3 @@
-from os import name
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -11,9 +10,13 @@ class TimeStamps:
 class Project(TimeStamps, models.Model):
     name = models.CharField(max_length=128, null=False)
     description = models.CharField(max_length=1024)
-    manager = models.OneToOneField(User)
-    developers = models.ManyToManyField(User)
+    manager = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='manager')
+    developers = models.ManyToManyField(User, related_name='developers')
     tags = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.name
 
 
 class Thread(TimeStamps, models.Model):
@@ -27,14 +30,18 @@ class Thread(TimeStamps, models.Model):
         ('enhancement', 'Enhancement'),
     )
 
-    bug_type = models.CharField(choices=BUG_TYPE_CHOICES, default='bug')
+    bug_type = models.CharField(
+        max_length=20, choices=BUG_TYPE_CHOICES, default='bug')
     title = models.CharField(max_length=256)
     description = models.CharField(max_length=1024)
     reporter = models.ForeignKey(User, on_delete=models.CASCADE)
-    bug_priority = models.CharField(
-        choices=BUG_PRIORITY_CHOICES, default='low')
+    bug_priority = models.CharField(max_length=20,
+                                    choices=BUG_PRIORITY_CHOICES, default='low')
     closed = models.BooleanField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(TimeStamps, models.Model):
@@ -44,6 +51,10 @@ class Comment(TimeStamps, models.Model):
         ('project_manager', 'Project Manager'),
     )
     body = models.CharField(max_length=1024)
-    author = models.OneToOneField(User)
-    author_role = models.CharField(choices=ROLE_CHOICES, default='contributor')
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    author_role = models.CharField(
+        max_length=20, choices=ROLE_CHOICES, default='contributor')
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.author + ':' + self.body
