4ee6eee55e7e6a40ba400c4b4399fe74166e3889
Yash Brahmkshatriya <yash311000@gmail.com>
2021-11-13 12:13:12 +0530

    Finish Home page

--- adf_bug_tracker/views.py
+++ adf_bug_tracker/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, template_name='index.html')
+
+# Home, Open/close of bug (by project manager)
+# Bug type, (by PM, not Reporter)
